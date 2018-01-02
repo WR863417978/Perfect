69c5cd87d39f8bcc5db0955945d76ea433dfdd9e
leidan1020 <1303761504@qq.com>
2018-01-02 15:15:51 +0800

    登录

---
+++ laravel/app/Http/Controllers/Home/LoginController.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use DB;
+
+/**
+* 前台登录注册控制器
+*/
+class LoginController extends Controller
+{
+	// 登录页面
+	public function login(Request $request)
+	{
+		if ($request->isMethod('post')) {
+			$data = $request->input('account');
+			print_r($data);
+			die;
+			echo 123;
+		}
+		else
+		{
+			return view('home.login');
+		}
+	}
+
+	// 注册页面
+}
